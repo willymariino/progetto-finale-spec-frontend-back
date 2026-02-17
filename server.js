dde85729b0b17a68ec01c95e3e630f3e3427aedb
willymariino <wg.mariino@gmail.com>
2026-02-17 18:08:24 +0100

    fix(server): update PORT definition to use environment variable for better configuration

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const app = express();
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
 // Middleware
 app.use(
